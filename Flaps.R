2ac184b34e92dd8226dce945fdd6560d1dfc9ead
KathyOchoa <112978531+KathyOchoa@users.noreply.github.com>
2023-11-05 21:43:33 -0500

    Add files via upload

--- Flaps.R
+++ Flaps.R
@@ -1,5 +1,6 @@
 # Necessary libraries
 library(tidyverse)
+library(tidyselect)
 library(dplyr)
 library(RColorBrewer)
 library(DataExplorer)
@@ -17,31 +18,31 @@ flaps2019 <- read.csv("2019_Flap.csv")
 ## Merge Flaps2016-Flaps2019 based on common columns
 # comCols used to merge flaps2016 and flaps2017
 # comCols2 used to merge flaps2016/2017 and 2018/2019
-comCols <- c("X.1", "X", "age", "age_neonate", "amonth", "aweekend", "died", 
-             "discwt", 
-              "dispuniform", "dqtr", "drg", "drgver", "drg_nopoa", "dxver",
-              "elective", "female", "hcup_ed", "hcup_division", "hosp_nis",
-              "i10_dx1", "i10_dx2", "i10_dx3", "i10_dx4", "i10_dx5", 
-              "i10_dx6","i10_dx7", "i10_dx8", "i10_dx9", "i10_dx10", 
-              "i10_dx11","i10_dx12","i10_dx13", "i10_dx14", "i10_dx15", 
-              "i10_dx16", "i10_dx17", "i10_dx18", "i10_dx19", "i10_dx20", 
-              "i10_dx21", "i10_dx22", "i10_dx23", "i10_dx24", "i10_dx25", 
-              "i10_dx26", "i10_dx27", "i10_dx28", "i10_dx29", "i10_dx30",
-              "i10_ndx", "i10_npr", 
-              "i10_pr1", "i10_pr2", "i10_pr3", "i10_pr4", "i10_pr5", 
-              "i10_pr6", "i10_pr7", "i10_pr8", "i10_pr9", "i10_pr10", 
-              "i10_pr11", "i10_pr12", "i10_pr13", "i10_pr14", "i10_pr15",
-              "key_nis", "los", "mdc", "mdc_nopoa", "nis_stratum",
-              "pay1", "pl_nchs",
-              "prday1", "prday2", "prday3", "prday4", "prday5", 
-              "prday6", "prday7", "prday8", "prday9", "prday10",
+comCols <- c("X.1", "X", "age", "age_neonate", "amonth", "aweekend", 
+             "died", "discwt", "dispuniform", "dqtr", "drg", "drgver", 
+             "drg_nopoa", "dxver","elective", "female", "hcup_ed", 
+             "hcup_division", "hosp_nis", 
+             "i10_dx1", "i10_dx2", "i10_dx3", "i10_dx4", "i10_dx5",
+             "i10_dx6","i10_dx7", "i10_dx8", "i10_dx9", "i10_dx10", 
+             "i10_dx11","i10_dx12","i10_dx13", "i10_dx14", "i10_dx15",
+             "i10_dx16", "i10_dx17", "i10_dx18", "i10_dx19", "i10_dx20",
+             "i10_dx21", "i10_dx22", "i10_dx23", "i10_dx24", "i10_dx25",
+             "i10_dx26", "i10_dx27", "i10_dx28", "i10_dx29", "i10_dx30",
+             "i10_ndx", "i10_npr",
+             "i10_pr1", "i10_pr2", "i10_pr3", "i10_pr4", "i10_pr5",
+             "i10_pr6", "i10_pr7", "i10_pr8", "i10_pr9", "i10_pr10",
+             "i10_pr11", "i10_pr12", "i10_pr13", "i10_pr14", "i10_pr15",
+             "key_nis", "los", "mdc", "mdc_nopoa", "nis_stratum",
+             "pay1", "pl_nchs",
+             "prday1", "prday2", "prday3", "prday4", "prday5",
+             "prday6", "prday7", "prday8", "prday9", "prday10",
              "prday11", "prday12", "prday13", "prday14", "prday15", 
-             "prver", "race", "totchg", "tran_in", "tran_out", "year",
-              "zipinc_qrtl", "flap")
-comcols2 <- c("X.1", "X", "age", "age_neonate", "amonth", "aweekend", "died", 
-              "discwt", 
-              "dispuniform", "dqtr", "drg", "drgver", "drg_nopoa",
-              "elective", "female", "hcup_ed", "hcup_division", "hosp_nis",
+             "prver", "race", "totchg", "tran_in", "tran_out", 
+             "year", "zipinc_qrtl", "flap")
+comcols2 <- c("X.1", "X", "age", "age_neonate", "amonth", "aweekend", 
+              "died", "discwt", "dispuniform", "dqtr", "drg", "drgver", 
+              "drg_nopoa", "elective", "female", "hcup_ed", 
+              "hcup_division", "hosp_nis",
               "i10_dx1", "i10_dx2", "i10_dx3", "i10_dx4", "i10_dx5", 
               "i10_dx6","i10_dx7", "i10_dx8", "i10_dx9", "i10_dx10", 
               "i10_dx11","i10_dx12","i10_dx13", "i10_dx14", "i10_dx15", 
@@ -58,7 +59,8 @@ comcols2 <- c("X.1", "X", "age", "age_neonate", "amonth", "aweekend", "died",
               "prday6", "prday7", "prday8", "prday9", "prday10",
               "prday11", "prday12", "prday13", "prday14", "prday15", 
               "race", "totchg", "tran_in", "tran_out", "year",
-              "zipinc_qrtl", "flap", "i10_dx31", "i10_dx32", "i10_dx33", "i10_dx34", "i10_dx35", 
+              "zipinc_qrtl", "flap", 
+              "i10_dx31", "i10_dx32", "i10_dx33", "i10_dx34", "i10_dx35", 
               "i10_dx36","i10_dx37", "i10_dx38", "i10_dx39", "i10_dx40",
               "i10_pr16", "i10_pr17", "i10_pr18", "i10_pr19", "i10_pr20", 
               "i10_pr21", "i10_pr22", "i10_pr23", "i10_pr24", "i10_pr25",
@@ -167,32 +169,12 @@ for(i in 1:length(incQt)) {
 head(categDf)
 head(dfJoins)
 
-# Procedure Codes
-procedureCols <- c("i10_pr1", "i10_pr2", "i10_pr3", "i10_pr4", 
-                   "i10_pr5", "i10_pr6", "i10_pr7", "i10_pr8", 
-                   "i10_pr9", "i10_pr10", "i10_pr11", "i10_pr12", 
-                   "i10_pr13", "i10_pr14", "i10_pr15","i10_pr16", 
-                   "i10_pr17", "i10_pr18", "i10_pr19", "i10_pr20", 
-                   "i10_pr21", "i10_pr22", "i10_pr23", "i10_pr24", 
-                   "i10_pr25")
-
-# Diagnostic Codes
-diagCols <- c("i10_dx1", "i10_dx2", "i10_dx3", "i10_dx4", "i10_dx5", 
-              "i10_dx6","i10_dx7", "i10_dx8", "i10_dx9", "i10_dx10", 
-              "i10_dx11","i10_dx12","i10_dx13", "i10_dx14", "i10_dx15", 
-              "i10_dx16", "i10_dx17", "i10_dx18", "i10_dx19", "i10_dx20", 
-              "i10_dx21", "i10_dx22", "i10_dx23", "i10_dx24", "i10_dx25", 
-              "i10_dx26", "i10_dx27", "i10_dx28", "i10_dx29", "i10_dx30",
-              "i10_dx31", "i10_dx32", "i10_dx33", "i10_dx34", "i10_dx35", 
-              "i10_dx36","i10_dx37", "i10_dx38", "i10_dx39", "i10_dx40")
 
 ### Exploratory Data Analysis ###
 
 # Frequency Table for categorical variables
 sapply(categDf, table)
 
-dfJoins %>%
-  count(i10_dx1)
 
 ### Statistical testing ###
 # Create a dataframe only for statistical testing
@@ -202,23 +184,52 @@ statTest <- dfJoins %>%
          between(elective,0,1), between(female,0,1))
 
 # Correlation plot
-library(DataExplorer)
 plot_correlation(statTest)
+#cor(statTest)
+
+# Multiple Regression Model
+ # Backwards elimination to analyze the relationship
+ # between total charge and several independent variables
+
+
 
 
 ### Visualizations ###
+
+# A - Annual Incidence #
+annualIncidence <- dfJoins %>%
+  select(flap, year) %>%
+  group_by(year) %>%
+  summarize(count = n()) %>%
+  arrange(year)
+
+ptAAnnualIncidence <- annualIncidence %>%
+  ggplot(aes(x = year, y = count)) +
+  ggtitle("Annual Flaps Incidence") +
+  theme_minimal(base_size = 12) + 
+  geom_line() +
+  xlab("Year") +
+  ylab("Annual Incidence")  +
+  scale_y_continuous(labels = label_number(suffix = " K", scale = 1e-3))
+  
+ptAAnnualIncidence
+
+
+# D - Mean Annual Charge ($) #
 meanAnnualCharge <- dfJoins %>%
   select(totchg, year) %>%
   filter(totchg > 0) %>%
   group_by(year) %>%
   summarize(avgCharge = mean(totchg)) %>%
   arrange(year)
-pt1MeanCharge <- meanAnnualCharge %>%
+
+ptDMeanCharge <- meanAnnualCharge %>%
   ggplot(aes(x = year, y = avgCharge)) +
-  ggtitle("Mean Annual Total Charges from 2016-2019") +
+  ggtitle("Mean Annual Total Charges of Flaps Procedures") +
   theme_minimal(base_size = 12) + 
   geom_line() +
   xlab("Year") +
-  ylab("Mean Annual Charge($)")
+  ylab("Mean Annual Charge($)") +
+  scale_y_continuous(labels = label_number(suffix = " K", scale = 1e-3))
 
-pt1MeanCharge
+ptDMeanCharge 
