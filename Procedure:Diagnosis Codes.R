fe4fde57211291c8efb4cad291862ee283dd5aaf
KathyOchoa <112978531+KathyOchoa@users.noreply.github.com>
2023-11-18 12:31:47 -0500

    Add files via upload

--- Procedure:Diagnosis Codes.R
+++ Procedure:Diagnosis Codes.R
@@ -13,8 +13,8 @@ flaps2019 <- read.csv("2019_Flap.csv", na = c("", "NA", "-99"))
 
 # Read in the excel sheet with all the necessary codes
 flapCodes <- read_excel("Timing to Flap Procedure Codes.xlsx")
-diagFracture <- read_excel("Timing to Flap Procedure Codes.xlsx", 2)
-operFixation <- read_excel("Timing to Flap Procedure Codes.xlsx", 3)
+fractureCodes <- read_excel("Timing to Flap Procedure Codes.xlsx", 2)
+fixationCodes <- read_excel("Timing to Flap Procedure Codes.xlsx", 3)
 
 
 ## Merge Flaps2016-Flaps2019 based on common columns
@@ -22,108 +22,118 @@ operFixation <- read_excel("Timing to Flap Procedure Codes.xlsx", 3)
 # comCols used to merge flaps2016 and flaps2017
 # comCols2 used to merge flaps2016/2017 and 2018/2019
 
-comCols <- c("X.1", "X", "age", "age_neonate", "amonth", "aweekend", 
-             "died", "discwt", "dispuniform", "dqtr", "drg", "drgver", 
-             "drg_nopoa","elective", "female", "hcup_ed", 
-             "hcup_division", "hosp_nis", 
-             "i10_dx1", "i10_dx2", "i10_dx3", "i10_dx4", "i10_dx5",
-             "i10_dx6","i10_dx7", "i10_dx8", "i10_dx9", "i10_dx10", 
-             "i10_dx11","i10_dx12","i10_dx13", "i10_dx14", "i10_dx15",
-             "i10_dx16", "i10_dx17", "i10_dx18", "i10_dx19", "i10_dx20",
-             "i10_dx21", "i10_dx22", "i10_dx23", "i10_dx24", "i10_dx25",
-             "i10_dx26", "i10_dx27", "i10_dx28", "i10_dx29", "i10_dx30",
-             "i10_ndx", "i10_npr",
-             "i10_pr1", "i10_pr2", "i10_pr3", "i10_pr4", "i10_pr5",
-             "i10_pr6", "i10_pr7", "i10_pr8", "i10_pr9", "i10_pr10",
-             "i10_pr11", "i10_pr12", "i10_pr13", "i10_pr14", "i10_pr15",
-             "key_nis", "los", "mdc", "mdc_nopoa", "nis_stratum",
-             "pay1", "pl_nchs",
-             "prday1", "prday2", "prday3", "prday4", "prday5",
-             "prday6", "prday7", "prday8", "prday9", "prday10",
-             "prday11", "prday12", "prday13", "prday14", "prday15", 
-             "race", "totchg", "tran_in", "tran_out", 
-             "year", "zipinc_qrtl", "flap")
-
-comcols2 <- c("i10_dx31", "i10_dx32", "i10_dx33", "i10_dx34", "i10_dx35", 
-              "i10_dx36","i10_dx37", "i10_dx38", "i10_dx39", "i10_dx40",
-              "i10_pr16", "i10_pr17", "i10_pr18", "i10_pr19", "i10_pr20", 
-              "i10_pr21", "i10_pr22", "i10_pr23", "i10_pr24", "i10_pr25",
-              "prday16", "prday17", "prday18", "prday19", "prday20",
-              "prday21", "prday22", "prday23", "prday24", "prday25")
+comCols1 <- intersect(names(flaps2016), names(flaps2017))
+comCols2 <- intersect(names(flaps2018), names(flaps2019))
 
 # Full join the 4 dataframes
-allYears <- flaps2016 %>%
-  full_join(flaps2017, by = c(comCols, "dxver", "prver")) %>%
-  full_join(flaps2018, by = c(comCols, comcols2)) %>%
-  full_join(flaps2019, by = c(comCols, comcols2))
 
+allYears <- flaps2016 %>%
+  full_join(flaps2017, by = comCols1) %>%
+  full_join(flaps2018, by = comCols2) %>%
+  full_join(flaps2019, by = comCols2)
+
+head(allYears)
 
 
 ### Diagnostic Codes ###
 
-diagCols <- c("i10_dx1", "i10_dx2", "i10_dx3", "i10_dx4", "i10_dx5", 
-              "i10_dx6","i10_dx7", "i10_dx8", "i10_dx9", "i10_dx10", 
-              "i10_dx11","i10_dx12","i10_dx13", "i10_dx14", "i10_dx15", 
-              "i10_dx16", "i10_dx17", "i10_dx18", "i10_dx19", "i10_dx20", 
-              "i10_dx21", "i10_dx22", "i10_dx23", "i10_dx24", "i10_dx25", 
-              "i10_dx26", "i10_dx27", "i10_dx28", "i10_dx29", "i10_dx30",
-              "i10_dx31", "i10_dx32", "i10_dx33", "i10_dx34", "i10_dx35", 
-              "i10_dx36","i10_dx37", "i10_dx38", "i10_dx39", "i10_dx40")
-
-
 # Subset only the diagnoses columns
 
 onlyDiagCols <- allYears %>%
-  select(diagCols, year)
+  select(year, contains("i10_dx"))
+
+head(onlyDiagCols)
 
 ## List all the associated diagnosis
 
-# Lengthen data, group by year then diagnosis code,
-# count each occurrence for each code, omit any NAs,
-# filter out diagnoses that occur less that 100 times,
-# arrange by descending order
-
-associatedDiagnosis <- pivot_longer(onlyDiagCols, diagCols) %>%
+diagnoses <- pivot_longer(onlyDiagCols, cols = contains("i10_dx")) %>%
   group_by(year, value) %>%
+  filter(value != "I10") %>%
   summarize(count = n()) %>%
   na.omit() %>%
-  filter(count > 100) %>%
-  filter(value != "I10") %>%
   arrange(year, desc(count))
 
+head(diagnoses)
 
-# Write associated Diagnosis to file
-write_csv(associatedDiagnosis, "associatedDiagnosis.csv")
+# Write diagnoses to file
+#write_csv(diagnoses, "associatedDiagnosis.csv")
 
 
 ## List the diagnostic fracture codes and their frequency
 
-# Lengthen data, group by year then diagnosis code,
-# filter the fracture codes, count the frequency 
-# omit any NAs, arrange by descending order
+diagnosticCM <- fractureCodes$`Diagnostic codes - CM codes`
 
-diagnosticCM <- diagFracture$`Diagnostic codes - CM codes`
-fractureCodes <- pivot_longer(onlyDiagCols, diagCols) %>%
-  group_by(year, value) %>%
-  filter(value %in% diagnosticCM) %>%
-  summarize(count = n()) %>%
-  na.omit() %>%
-  arrange(year, desc(count))
+onlyFracture <- diagnoses %>%
+  filter(value %in% diagnosticCM)
+
+head(onlyFracture)
+
+# Rename the columns
+colnames(fractureCodes)[1] = "cm_code"
+colnames(fractureCodes)[2] = "description"
+colnames(onlyFracture)[2] = "cm_code"
+
+# Merge the onlyFracture df with the fractureCodes df
+frac_desc <- merge(onlyFracture, fractureCodes, sort=FALSE)
+head(frac_desc)
   
+# Write the fractureCodes to file
+#write_csv(frac_desc, "diagnosticFracture.csv")
+
 
 
 ### Procedure Codes ###
 
-procedureCols <- c("i10_pr1", "i10_pr2", "i10_pr3", "i10_pr4", 
-                   "i10_pr5", "i10_pr6", "i10_pr7", "i10_pr8", 
-                   "i10_pr9", "i10_pr10", "i10_pr11", "i10_pr12", 
-                   "i10_pr13", "i10_pr14", "i10_pr15","i10_pr16", 
-                   "i10_pr17", "i10_pr18", "i10_pr19", "i10_pr20", 
-                   "i10_pr21", "i10_pr22", "i10_pr23", "i10_pr24", 
-                   "i10_pr25")
+# Subset only the procedure columns
 
-# Create flapTypes df of procedure codes
+onlyProcedureCols <- allYears %>%
+  select(year, starts_with("i10_pr"))
 
-flapTypes <- allYears %>%
-  select(year, procedureCols)
+head(onlyProcedureCols)
 
+# Subset FLAP procedures
+
+flapCodes1 <- flapCodes$`Flap coding`
+
+flapProcedures <- pivot_longer(onlyProcedureCols, cols = contains("i10_pr")) %>%
+  group_by(year, value) %>%
+  summarize(count = n()) %>%
+  na.omit() %>%
+  filter(value %in% c(flapCodes1)) %>%
+  arrange(year, desc(count))
+
+head(flapProcedures)
+
+# Rename columns
+colnames(flapCodes)[1] <-"flap_code"
+colnames(flapCodes)[2] <- "description"
+colnames(flapProcedures)[2] <- "flap_code"
+
+# Merge the flapProcedures df with the flapCodes df
+flap_desc <- merge(flapProcedures, flapCodes, sort = FALSE)
+
+head(flap_desc)
+
+
+# Subset FIXATION procedures
+
+fixationCodes1 <- fixationCodes$`Procedural Codes`
+
+fixationProcedures <- pivot_longer(onlyProcedureCols, cols = contains("i10_pr")) %>%
+  group_by(year, value) %>%
+  summarize(count = n()) %>%
+  na.omit() %>%
+  filter(value %in% c(fixationCodes1)) %>%
+  arrange(year, desc(count))
+
+head(fixationProcedures)
+
+# Rename columns
+colnames(fixationCodes)[1] <-"fixation_code"
+colnames(fixationCodes)[2] <- "description"
+colnames(fixationProcedures)[2] <- "fixation_code"
+
+# Merge the flapProcedures df with the flapCodes df
+fixation_desc <- merge(fixationProcedures, fixationCodes, sort = FALSE)
+
+head(fixation_desc)
